7ecdb1cf8713dbf5c23413c44ef98e94cc68ca37
CrypticCoder2502 <43320248+CrypticCoder2502@users.noreply.github.com>
2019-04-05 23:01:16 +0530

    123

--- index.js
+++ index.js
@@ -2,10 +2,56 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
   	document.getElementById("status").innerHTML = "Hello " + user.displayName;
   	document.getElementById("logout").innerHTML = "Log Out";
+  	var verify = user.emailVerified;
+	if(verify){
+		document.getElementById("event1").style.display = "block";
+		document.getElementById("event2").style.display = "block";
+		document.getElementById("event3").style.display = "block";
+		document.getElementById("event4").style.display = "block";
+		document.getElementById("event5").style.display = "block";
+		document.getElementById("event6").style.display = "block";
+		document.getElementById("event7").style.display = "block";
+		document.getElementById("event8").style.display = "block";
+		document.getElementById("event9").style.display = "block";
+		document.getElementById("event10").style.display = "block";
+		document.getElementById("event11").style.display = "block";
+		document.getElementById("event12").style.display = "block";
+		
+	}
+	else{
+		document.getElementById("event1").style.display = "none";
+		document.getElementById("event2").style.display = "none";
+		document.getElementById("event3").style.display = "none";
+		document.getElementById("event4").style.display = "none";
+		document.getElementById("event5").style.display = "none";
+		document.getElementById("event6").style.display = "none";
+		document.getElementById("event7").style.display = "none";
+		document.getElementById("event8").style.display = "none";
+		document.getElementById("event9").style.display = "none";
+		document.getElementById("event10").style.display = "none";
+		document.getElementById("event11").style.display = "none";
+		document.getElementById("event12").style.display = "none";
+
+	
+	}
     // User is signed in.
   } else {
-  	document.getElementById("status").innerHTML = "Register/SignIn";
+  	document.getElementById("status").innerHTML = "SignUp/Login";
   	document.getElementById("logout").innerHTML = "";
+  		document.getElementById("event1").style.display = "none";
+		document.getElementById("event2").style.display = "none";
+		document.getElementById("event3").style.display = "none";
+		document.getElementById("event4").style.display = "none";
+		document.getElementById("event5").style.display = "none";
+		document.getElementById("event6").style.display = "none";
+		document.getElementById("event7").style.display = "none";
+		document.getElementById("event8").style.display = "none";
+		document.getElementById("event9").style.display = "none";
+		document.getElementById("event10").style.display = "none";
+		document.getElementById("event11").style.display = "none";
+		document.getElementById("event12").style.display = "none";
+
+
     // No user is signed in.
   }
 });
@@ -19,254 +65,3 @@ function logout(){
 	});
 }
 
-function event1_registration(){
-
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Hackathon");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Hackathon registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event2_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Business_Quiz");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Business_Quiz registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event3_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Video_Advertising_Competition");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Video_Advertising_Competition registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event4_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Game_Of_Thrones");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Game_Of_Thrones registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event5_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Sales_Pitching");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Sales_Pitching registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event6_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Campaging");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Campaging registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event7_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Pitching");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Pitching registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-function event8_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Binder");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Binder registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-
-function event9_registration(){
-	
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("IPL");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("IPL registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
-
-function workshop_registration(){
-	var user = firebase.auth().currentUser;
-
-	if (user!= null){
-		var verify = user.emailVerified;
-		if(verify){
-			var database = firebase.database();
-			var ref = database.ref("Workshop");
-			var data = {
-				name: user.displayName,
-				email: user.email
-			}
-			ref.push(data);
-			window.alert("Workshop registration Complete, Please see other events too !!");
-		}
-		else{
-			window.alert("Please, Verify your E-mail");
-		}
-	}
-
-	else{
-		window.alert("First Register");
-	}
-}
